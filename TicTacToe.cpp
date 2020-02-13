f1dfa9b1f73ebb38da0d74c8cfea535273a35d87
Sam-Goulding <gouldingsam@gmail.com>
2020-02-13 16:47:24 -0700

    Added DisplayBoard() Function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
+
 using namespace std;
 
 /**
- * Create a 3x3 multidimentional array to act as a board
+ * Create a 3x3 multidimensional array to act as a board
  * 
  * @return The new board array
  */
@@ -20,9 +21,29 @@ string** CreateBoard()
     return new_board;
 }
 
+/**
+ * Display the 3x3 multidimensional array to the user
+ * Input is the 3x3 board to display
+
+ **/
+
+void DisplayBoard(string** board)
+{
+    cout<<"The current board is: "<<endl;
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            cout<<board[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 int main()
 {
     string** board = CreateBoard();
+    DisplayBoard(board);
 
     return 0;
 }
